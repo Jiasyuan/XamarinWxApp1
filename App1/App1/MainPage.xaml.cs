26d315086e03529141b3bfa7cd651fbc353a6db9
JiaSy <JiaSy@JIASYUANU38DT>
2018-11-28 22:38:10 +0800

    加入專案檔案。

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ClassLibrary1.ClassLibrary1 csl = new ClassLibrary1.ClassLibrary1();
+            int count = csl.add(4,7);
+            //lb= count.ToString();
+        }
+    }
+}
